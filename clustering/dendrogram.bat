7550451c39d795146589b7ffd72104a6c33d5336
TakeshiMineishi <minegishi.takeshi@ark-info-sys.co.jp>
2021-11-19 09:35:36 +0900

    パス等の修正

--- clustering/dendrogram.bat
+++ clustering/dendrogram.bat
@@ -1,4 +1,4 @@
-@echo off
+@rem @echo off
 
 @rem ===================================================================
 @rem 処理内容：
@@ -7,17 +7,19 @@
 @rem 　dendrogram.bat 入力ファイル名 出力ファイル名 分割クラス数
 @rem ===================================================================
 
+echo dendrogram.bat %1 %2 %3
+
 @rem カレントディレクトリの保存
 set cwd=%~dp0
 
 @rem Anaconda環境（Python）の呼び出し
-call C:\Users\mine\anaconda3\Scripts\activate.bat
+call C:\Users\hirotec\anaconda3\Scripts\activate.bat
 call conda activate hirotec
 cd "D:\WeldEvaluation\python"
 d:
 
 @rem コマンドの実行
-python dendrogram.py --input_file %1 --outpurt_file %2 --num_classes %3
+python dendrogram.py --input_file %1 --output_file %2 --num_classes %3
 echo %ERRORLEVEL% > return.txt
 
 @rem Anaconda環境の解放
